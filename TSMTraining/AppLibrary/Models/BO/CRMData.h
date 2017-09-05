4464df748cf67560337f4fe22fa02aa5f3ce87dc
praveen kumar <praveen@mobikasa.com>
2017-09-05 18:36:43 +0530

    oflinedata

---
+++ TSMTraining/AppLibrary/Models/BO/CRMData.h
@@ -0,0 +1,36 @@
+//
+//  CRMData.h
+//  TSMTraining
+//
+//  Created by Mobikasa on 05/09/17.
+//
+//
+
+#import <JSONModel/JSONModel.h>
+
+@protocol CRMData
+@end
+
+@interface CRMData : JSONModel
+
+@property (nonatomic,strong) NSString *crm_LOB;
+@property (nonatomic,strong) NSString *crm_designation;
+@property (nonatomic,strong) NSString *crm_id;
+@property (nonatomic,strong) NSString *crm_name;
+@property (nonatomic,strong) NSString *crm_password;
+@property (nonatomic,strong) NSString *crm_product_line;
+@property (nonatomic,strong) NSString *crm_status;
+@property (nonatomic,strong) NSString *dealer_code;
+@property (nonatomic,strong) NSString *dealer_location;
+@property (nonatomic,strong) NSString *dealer_name;
+@property (nonatomic,strong) NSString *last_reg_update;
+@property (nonatomic,strong) NSString *region;
+@property (nonatomic,strong) NSString *state_office;
+
+@end
+
+@interface CRMDataArray : JSONModel
+
+@property (nonatomic, strong) NSMutableArray<CRMData> *data;
+
+@end
---
+++ TSMTraining/AppLibrary/Models/BO/CRMData.m
@@ -0,0 +1,36 @@
+//
+//  CRMData.m
+//  TSMTraining
+//
+//  Created by Mobikasa on 05/09/17.
+//
+//
+
+#import "CRMData.h"
+
+@implementation CRMData
+
++(BOOL)propertyIsOptional:(NSString *)propertyName{
+    
+    return YES;
+}
+@end
+
+@implementation CRMDataArray
+
++(BOOL)propertyIsOptional:(NSString *)propertyName{
+    return YES;
+}
+
+-(id)init{
+    
+    self = [super init];
+    
+    if (self) {
+        self.data = (id)[NSMutableArray new];
+    }
+    
+    return self;
+}
+
+@end
